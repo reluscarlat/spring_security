2bf0824947222aeb25c562933cb0aa75acbeb339
relu <relu.scarlat@gmail.com>
2019-09-20 16:51:03 +0300

    Commit 0.5

---
+++ springsecurityauthorizationserver101/src/main/java/com/resc/springsecurity101authorizationserver/Springsecurity101authorizationserverApplication.java
@@ -0,0 +1,13 @@
+package com.resc.springsecurity101authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurity101authorizationserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springsecurity101authorizationserverApplication.class, args);
+    }
+
+}
---
+++ springsecurityauthorizationserver101/src/main/java/com/resc/springsecurity101authorizationserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,33 @@
+package com.resc.springsecurity101authorizationserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+@EnableResourceServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {  // Tells which are the clients applications that can use the responsibility of authorization server
+        clients.inMemory()
+                .withClient("clientId")
+                .secret("abcde")
+                .authorizedGrantTypes("password","refresh_token","authorization_token")
+                .scopes("write");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
